e14d7bac87bda2ffe8324b7f6f6ebe2f485eb315
Rathnavel-Rat <rathnavel8026@gmail.com>
2021-03-25 22:19:41 +0530

    #1 from with page

--- src/Components/RespondToForm/RespondToForm.jsx
+++ src/Components/RespondToForm/RespondToForm.jsx
@@ -1,16 +1,15 @@
 import React,{useState} from 'react';
-import {Container, Form, Grid, List, Segment} from 'semantic-ui-react'
+import {Container, Divider, Form, Grid, List, Segment} from 'semantic-ui-react'
 import {useForm,Controller, FormProvider} from "react-hook-form";
 import Axios from "../axiosConfig";
 import {base64ToArrayBuffer} from "../CreateForms/utils";
-import {ListFields} from "../CreateForms/protobuf/Fields_pb";
+import {ListPageForm} from "../CreateForms/protobuf/Fields_pb";
 import {RenderProto} from "../CreateForms/Fields/RenderFields";
 
 
 const RespondToForm = () => {
-    const [FormElements,setFormElements]=useState([])
+    const [PageForm,setPageForm]=useState([])
     const {register,handleSubmit,errors,control}= useForm()
-
     const Search=(e)=>{
         Axios().get("dynamicforms/AccessForm", {
             params: {
@@ -18,67 +17,66 @@ const RespondToForm = () => {
             }
         }).then(e=>{
             const data= base64ToArrayBuffer(e.data.data)
-            setFormElements(ListFields.deserializeBinary(data).getFieldsList())
+            setPageForm(ListPageForm.deserializeBinary(data).getPageList())
         }).catch(e=>{
 
         })
     }
 
+
     return (
         <div>
-            <Grid >
-            <Grid.Row columns={1}  textAlign={"center"} verticalAlign={"middle"}>
-                <Grid.Column>
-                    <Container textAlign={"center"}>
-                <Form onSubmit={handleSubmit(Search)}>
+            <Grid textAlign="center" verticalAlign="middle">
+                <Grid.Column style={{ maxWidth: "450px" }} >
+                    <Container fluid textAlign={"center"} text>
+                <Form  onSubmit={handleSubmit(Search)}>
                 <Controller as={Form.Input} control={control}
-                            width={5}  ref={register({required:{value:true,message:"Please enter a valid id"}})}
+                            ref={register({required:{value:true,message:"Please enter a valid id"}})}
                             name="key"
                             type="text" placeholder="enter the form access-id" />
                     {errors["key"] && errors["key"].message}
                 <Form.Button  type={"submit"}  >Get</Form.Button>
                  </Form></Container>
-
                 </Grid.Column>
-            </Grid.Row>
             </Grid>
-
-                <Grid celled stackable doubling >
-                    <Grid.Row  stretched columns={2}>
-
-                        <Grid.Column  computer={3} >
-
-                        </Grid.Column >
-
-                        <Grid.Column  computer={8} >
-                            <FromFrame data={FormElements}/>
-                        </Grid.Column>
-
-                    </Grid.Row>
-                </Grid>
-
+            <Grid textAlign="center" verticalAlign="middle" >
+                <Grid.Column style={{ maxWidth: "900px" }} text>
+               <FormDisplay data={PageForm}/>
+                </Grid.Column>
+            </Grid>
         </div>
     );
 };
 
 export default RespondToForm;
-const FromFrame=({data})=>{
-    const methods=useForm()
 
+const FormDisplay=({data})=>{
+    const methods=useForm()
     const submit=(e)=>{
         console.log(e,"kkk")
     }
     return(
-        <List>
+
         <FormProvider {...methods} >
-            <Form onSubmit={methods.handleSubmit(submit)} width={"equal"}>
+                    <Form onSubmit={methods.handleSubmit(submit)} width={"equal"}>
+                        {data.map((e,i)=>(
+                            <FromFrame  data={e.getFieldsList()}/>
+                        ))}
+                        <Form.Button content="submit" primary  />
+                    </Form>
+        </FormProvider>
+    )
+}
+
+const FromFrame=({data,})=>{
+    return(
+        <Segment>
+        <List>
                 {data.map((e,i)=>(
                     <RenderEachItem item={e} key={i}/>
                 ))}
-                <Form.Button content="submit" primary  />
-            </Form>
-        </FormProvider>
         </List>
+        </Segment>
     )
 }
 
@@ -86,8 +84,9 @@ const RenderEachItem=({item})=>{
     const nRenderProto = new RenderProto();
     const field = nRenderProto[String(item.getRenderFunc())](item);
     return(
-        <List.Item as={Segment} textAlign="left">
+        <List.Item as={Container}  textAlign="left">
             {field}
+            <Divider/>
         </List.Item>
 
     )
